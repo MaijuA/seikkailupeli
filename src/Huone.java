c4dc9d395ed1b3a54c5bbf85d9cc972809202901
jsuhonen <jakesuhonen@gmail.com>
2017-10-17 12:02:57 +0300

    huone

--- src/Huone.java
+++ src/Huone.java
@@ -1,2 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Huone {
+    private int id;
+    private String nimi;
+    private String kuvaus;
+//    String tavarakuvaus;
+
+    List<Tavara> tavaralista = new ArrayList<Tavara>();
+
+
+
+    public Huone(int id, String nimi, String kuvaus) {
+    this.id = id;
+    this.nimi = nimi;
+    this.kuvaus = kuvaus;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNimi() {
+        return nimi;
+    }
+
+    public String getKuvaus() {
+        return kuvaus;
+    }
+
+    public Tavara getTavarakuvaus() {
+
+        return tavaralista.get(0);
+    }
 }
